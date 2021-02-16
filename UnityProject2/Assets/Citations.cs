4a0b37866910e06db404d099ffb139a6affb168b
Jake Nordin <jacob.nordin4699@gmail.com>
2021-02-16 15:22:39 -0600

    Added more background, created GameManager, Canvas with diaglog box

---
+++ UnityProject2/Assets/Citations.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Citations : MonoBehaviour
+{
+    // font : https://www.dafont.com/some-time-later.font?l[]=10
+    // salt enemy : https://www.pngrepo.com/svg/227326/salt
+}
